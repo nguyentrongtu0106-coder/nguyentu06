8417fd0976898d7cf34deff9d8fa667a8efe5931
nguyentrongtu0106-coder <nguyentrongtu0106@gmail.com>
2025-12-08 00:01:00 +0700

    Delete BAI 4 directory

--- BAI 4/bai4_15.py
+++
@@ -1,8 +0,0 @@
-print ("Sinh vien: NGUYEN TRONG TU")
-print ("Ma so sv: 245752021610051")
-chuoi = input("Nhập chuỗi các từ tiếng Anh, cách nhau bằng dấu cách: ")
-ds_tu = chuoi.split()
-ds_tu.sort()
-print("Các từ sau khi sắp xếp theo thứ tự từ điển là:")
-for tu in ds_tu:
-    print(tu)
